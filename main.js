03ca62be8dce86fe9c818b7c622a46c644657203
Przemysław Soliński <solinskip107@gmail.com>
2023-01-17 13:47:13 +0100

    Simple heart animation

---
+++ main.js
@@ -0,0 +1,8 @@
+const text = document.getElementById('text');
+const heart = document.getElementById('heart');
+const x = window.innerWidth / 2 - 50;
+const y = window.innerHeight / 2 - 75;
+text.style.left = x + 'px';
+text.style.top = y + 'px';
+heart.style.left = x + 'px';
+heart.style.top = y + 'px';
